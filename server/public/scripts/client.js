f54f0bac908ce70eb911b852e53824ee579b2ce8
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-14 11:52:08 -0500

    help from Steven and Max

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,8 @@
+const calcHx = require("../../modules/calcHx");
+
 $(document).ready(onReady);
+
+let operators = ""
 
 function onReady() {
   console.log("jquery is loaded");
@@ -15,12 +19,13 @@ function post2Server(event) {
   event.preventDefault();
   let inputA = $("#inputA").val();
   let inputB = $("#inputB").val();
-  "#inputA".val("");
-  "#inputB".val("");
+  
+    console.log(inputA, operators, inputB);
+
 
   $.ajax({
     method: "POST",
-    url: "/calcHx",
+    url: "/mathCalcs",
     data: {
       inputOne: inputA,
       inputTwo: inputB,
@@ -36,12 +41,16 @@ function post2Server(event) {
     });
 } //end of post2Server
 
+//missing something in the middle i think
+
+
 function getFromServer() {
   $.ajax({
     method: "GET",
-    url: "/input",
+    url: "/mathCalcs",
   })
     .then(function (response) {
+        console.log(response);
       console.log("omg! get from server is working?!");
     })
     .catch(function (error) {
@@ -50,52 +59,92 @@ function getFromServer() {
     });
 } //end of get from server
 
-function renderToDom(gotData){
-    console.log('Got DaTA!');
-    $('#mathHistory').append(`
-        <li>${gotData[gotData.length -1].inputA}</li>
-    `)
-
-    
-}//end of render to DOM
 
 
+
+//is this the part that is posting the data that we got?
+function renderToDom(gotData) {
+  console.log("Got Data!");
+  $("#calcHx").append(`
+        <li>${calcHx[calcHx.length - 1].inputA, Number(operators), calcHx[calcHx.length - 1].inputB}</li>
+    `);
+} //end of render to DOM
 
 //FUNCTIONS for the items above
 
 function addFunc() {
-    event.preventDefault();
+  event.preventDefault();
   console.log("addFunc is working");
+  operators = "+"
+  console.log(operators)
   inputA + inputB;
 }
 
 function subFunc() {
-    event.preventDefault();
+  event.preventDefault();
   console.log("subFunc is working");
+  operators = "-"
+  console.log(operators)
   inputA - inputB;
 }
 
 function multiFunc() {
-    event.preventDefault();
+  event.preventDefault();
   console.log("multiFunc is working");
+  operators = "*"
+  console.log(operators)
   inputA * inputB;
 }
 
 function divFunc() {
-    event.preventDefault();
+  event.preventDefault();
+  operators = "/"
+  console.log(operators)
   console.log("divFunc is working");
   inputA / inputB;
 }
 
 function submitFunc() {
-    event.preventDefault();
+  event.preventDefault();
   console.log("submitFunc is working");
+  post2Server();
 }
 
 function clearFunc() {
   console.log("clearFunc is working");
 }
 
+
+//i want this to be my response 
+function post2Server(response) {
+    console.log("response is ready");
+    event.preventDefault();
+    let inputA = $("#inputA").val();
+    let inputB = $("#inputB").val();
+    console.log(inputA, inputB);
+    $("#inputA").val("");
+    $("#inputB").val("");
+  
+    $.ajax({
+      method: "POST",
+      url: "/mathCalcs",
+      data: {
+        inputOne: inputA,
+        inputTwo: inputB,
+      } //end of data
+    })
+      .then(function (response) {
+        console.log("nice, working so far!");
+        getFromServer();
+      })
+      .catch(function (error) {
+        alert("ah shit catch-ed this thing wrong");
+        console.log("Request failed", error);
+      });
+  } //end of post2Server
+// //
+//  $('#inputA).val("");
+//   "#inputB".val("");
 //EVENT HANDLER
 
 //DEFAULT EVENT PREVENT
--- server/server.js
+++ server/server.js
@@ -2,8 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-
 let mathCalcs = [];
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+
+app.get('/mathCalcs', function(req, res) {
+    console.log('Request for /mathCalcs was made');
+  
+    // send back list of quotes to client
+    res.send(mathCalcs);
+    
+    // If I want to send an error: 
+    // res.sendStatus(500);
+  });
+  
+  // Post route
+  app.post('/mathCalcs', function(req, res) {
+    // req.body is the data that the client has sent in the request
+    // req.body is a thing we get from bodyParser
+    console.log('POST some data!', req.body);
+  
+    // Add my new quote to the quoteList
+    mathCalcs.push(req.body);
+  
+  
+    // The server always needs to respond!
+    // Status 201 = 'Created'
+    res.sendStatus(201);
+  
+  })
+
+
 
 function mathing(){
 
@@ -39,10 +72,7 @@ function addFunc() {
   }
 
 
-app.use(bodyParser.urlencoded({extended:true}))
 
-// Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
 
 
 
