ea14ac41b551e55c4fd350a47c4e5b1e08337c91
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-15 15:40:54 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.port || 5000;
 
 const calculator = require("./modules/calculator");
 
